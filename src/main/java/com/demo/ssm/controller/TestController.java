45b95be253ef7edcfcc0993e3d300f7aaf50cfb6
zhanghw_zhangsh@126.com <zhanghw_zhangsh@126.com>
2019-09-24 18:29:03 +0800

    修改提交

--- src/main/java/com/demo/ssm/controller/TestController.java
+++ src/main/java/com/demo/ssm/controller/TestController.java
@@ -1,12 +1,11 @@
 package com.demo.ssm.controller;
 
+import com.demo.ssm.bean.Deptarment;
+import com.demo.ssm.dao.DeptarmentMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.demo.ssm.bean.Deptarment;
-import com.demo.ssm.dao.DeptarmentMapper;
 
 @Controller
 @RequestMapping(value = "test",produces = "text/plain;charset=utf-8")
